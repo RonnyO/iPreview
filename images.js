60526798bfa89b7ec4774849928dfd39ef837eec
Ronny Orbach <r@ronnyo.com>
2011-03-13 10:11:33 +0200

    conforming to jQuery standards, take two

--- images.js
+++ images.js
@@ -1,33 +1,39 @@
 (function( $ ){
-  $.fn.images = function( options ){
-  
-	var that = this;
-	
+  $.fn.images = function( method ){
+	var that = {preview: null};
     var settings = {
 		previewPath: 'preview/'	  
     };
 	
 	var methods = {
-		init: function(){
-			that.$preview = $('<div id="imagePreview">').appendTo('#gallery');
-		}(),
+		init: function( options ){
+			if ( options ) { 
+				$.extend(settings, options);
+			}
+			that.preview = $('<div id="imagePreview">').appendTo('#gallery');
+			
+			return $(this).each(function(){
+				$(this).hover(methods.show, methods.hide);
+			});
+		},
 		show: function(ev){
-			$(this).find('.details').clone().appendTo(that.$preview);
+			$(this).find('.details').clone().appendTo(that.preview);
 			console.log($(this).offset());
-			that.$preview.animate({});
+			that.preview.animate({});
 		},
 		hide: function(ev){
-			that.$preview.empty();
+			that.preview.empty();
 		},
 		resize: function(){}
 	};
 	
-    return $(this).each(function(){        
-      if ( options ) { 
-        $.extend(settings, options);
-      }
-	  
-	  $(this).hover(methods.show, methods.hide);
-    });
+	// Method calling logic
+	if ( methods[method] ) {
+		return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+	} else if ( typeof method === 'object' || ! method ) {
+		return methods.init.apply( this, arguments );
+	} else {
+		$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+	}
   };
 })( jQuery );
