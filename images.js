9d996c17d698c22076d66faad9bbe9909584b09a
Ronny Orbach <r@ronnyo.com>
2011-03-13 14:01:04 +0200

    basic preview

--- images.js
+++ images.js
@@ -1,19 +1,20 @@
 (function( $ ){
 	var that = {preview: null};
     var settings = {
-		previewPath: 'preview/'	  
+		previewPath: 'preview/'
     };
 	
+
 	var methods = {
 		init: function( options ){
 			if ( options ) { 
 				$.extend(settings, options);
 			}
-			that.preview = $('<div id="imagePreview">').appendTo('#gallery');
+			that.preview = $('<div id="imagePreview">').appendTo('body');
+			that.preview.bind('mouseleave.images', methods.hide);
 			
 			return $(this).each(function(){
-				$(this).bind('mouseenter.images', methods.show);
-				$(this).bind('mouseleave.images', methods.hide);
+				$(this).bind('mouseover.images', methods.show);
 			});
 		},
 		destroy: function(){
@@ -21,12 +22,19 @@
 			if ( that.preview ) that.preview.remove();
 		},
 		show: function(ev){
-			$(this).find('.details').clone().appendTo(that.preview);
-			console.log($(this).offset());
-			that.preview.animate({});
+			//methods.hide();
+			console.log('showing');
+			$(this).clone().appendTo(that.preview);
+			
+			that.preview.show().css({
+				top: $(this).position().top + 1,
+				left: $(this).position().left - 3
+			})
+			.animate({});
 		},
-		hide: function(ev){
-			that.preview.empty();
+		hide: function(){
+			console.log('hiding');
+			that.preview.hide().empty();
 		},
 		resize: function(){}
 	};
@@ -37,7 +45,7 @@
 	} else if ( typeof method === 'object' || ! method ) {
 		return methods.init.apply( this, arguments );
 	} else {
-		$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+		$.error( 'Method ' +  method + ' does not exist on jQuery.images' );
 	}
   };
 })( jQuery );
