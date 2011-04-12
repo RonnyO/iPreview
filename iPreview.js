806269e16746ca2f73e3a29acab264a4dd07c747
Ronny Orbach <r@ronnyo.com>
2011-04-12 18:48:06 +0300

    fix IE bug

--- iPreview.js
+++ iPreview.js
@@ -49,10 +49,9 @@
 				// take into consideration tooltip's padding (7)
 				previewLeft = ($this.position().left - 3) - (previewWidth - width) / 2 - 7,
 				// mask covers the original image and is unified to the tooltip for the mouseleave event
-				maskTop = $this.offset().top - $(window).scrollTop(),	// $(window).scrollTop() --> distance between the viewport's current top and the begining of the document
-				maskLeft = $this.offset().left - $(window).scrollLeft();// $(window).scrollLeft() --> distance between the viewport's current left and the begining of the row
-//			previewTop = Math.min(Math.max(previewTop, 13, $(window).scrollTop() + 13), $(window).scrollTop() + $(window).innerHeight() - previewHeight - 18);
-			previewTop = Math.min(Math.max(previewTop, 13, window.pageYOffset + 13), window.pageYOffset + window.innerHeight - previewHeight - 18);
+				maskTop = $this.offset().top - $(window).scrollTop(),
+				maskLeft = $this.offset().left - $(window).scrollLeft();
+			previewTop = Math.min(Math.max(previewTop, 13, $(window).scrollTop() + 13), $(window).scrollTop() + $(window).height() - previewHeight - 18);
             previewLeft = Math.max(previewLeft, 13);
 			var widthDelta = (previewLeft + previewWidth + 13) - $(window).width();
 			if (widthDelta > 0) previewLeft -= widthDelta + 15;
